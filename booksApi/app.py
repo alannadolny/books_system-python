6482ba4c306bdbabe5590b0aa57602cc8843aa60
Alan Nadolny <alannadolnyy@gmail.com>
2022-07-30 00:05:51 +0200

    add validation and put endpoint

--- booksApi/app.py
+++ booksApi/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, abort
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import json
 
 client = MongoClient('mongodb://localhost:27017/')
+validator = json.load(open('validation.json'))
+client.books.command('collMod', 'collection', validator=validator, validationLevel='moderate')
 app = Flask(__name__)
 
 
@@ -30,11 +33,25 @@ def post_book():
         return abort(500)
 
 
-@app.route("/books/<idToDelete>", methods=['DELETE'])
+@app.route("/books/<id_to_delete>", methods=['DELETE'])
 def delete_book(id_to_delete):
     try:
         client.books.collection.delete_one({'_id': ObjectId(id_to_delete)})
         return {'deletedId': id_to_delete}
+    except ():
+        return abort(500)
+
+
+@app.route("/books/<id_to_update>", methods=['PUT'])
+def update_book(id_to_update):
+    try:
+        content_type = request.headers.get('Content-Type')
+        if content_type == 'application/json':
+            client.books.collection.find_one_and_update({"_id": ObjectId(id_to_update)},
+                                                        {'$set': request.json})
+            return str(request.json)
+        else:
+            return 'Content-Type not supported!'
     except ():
         return abort(500)
 
